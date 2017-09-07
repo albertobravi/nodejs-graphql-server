74c34b652eb473aaae0ba2531efa980a9eff1b3d
Alberto Bravi <alberto.bravi@gmail.com>
2017-09-07 12:40:20 +0200

    Implemented update and delete mutations

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -7,6 +7,8 @@ mongoose.connect(process.env.MONGO_URL, {
   useMongoClient: true,
 });
 
+mongoose.Promise = global.Promise;
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
