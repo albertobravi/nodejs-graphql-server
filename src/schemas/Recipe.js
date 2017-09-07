974e9fbd83d8eb3370e519436216be4e4462169b
Alberto Bravi <alberto.bravi@gmail.com>
2017-09-07 12:40:20 +0200

    Implemented update and delete mutations

--- src/schemas/Recipe.js
+++ src/schemas/Recipe.js
@@ -1,11 +1,26 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import RecipeLoaders from '../dataloaders/Recipe';
+import RecipeModel from '../models/Recipe';
 
 const typeDefs = [`
 type Recipe {
-  _id: ID!
+  id: ID!
   title: String!
   body: String
+}
+
+type RecipeStatus {
+  message: String!
+}
+
+input UpdateRecipeInput {
+  id: ID!
+  title: String
+  body: String
+}
+
+input DeleteRecipeInput {
+  id: ID!
 }
 
 type Query {
@@ -13,14 +28,50 @@ type Query {
   recipeByTitle(title: String!): Recipe
 }
 
+type Mutation {
+  updateRecipe(input: UpdateRecipeInput!): Recipe
+  deleteRecipe(input: DeleteRecipeInput!): RecipeStatus
+}
+
 schema {
-  query: Query
-}`];
+  query: Query,
+  mutation: Mutation,
+}
+`];
 
 const resolvers = {
   Query: {
     recipeById: (_, { id }) => RecipeLoaders.ids.load(id),
     recipeByTitle: (_, { title }) => RecipeLoaders.titles.load(title),
+  },
+  Mutation: {
+    updateRecipe: (_, UpdateRecipeInput) => {
+      return RecipeModel.findById(UpdateRecipeInput.input.id)
+        .exec()
+        .then((recipe) => {
+          recipe.title = UpdateRecipeInput.input.title || recipe.title;
+          recipe.body = UpdateRecipeInput.input.body || recipe.body;
+
+          return recipe.save();
+        })
+        .then(null, err => err);
+    },
+    deleteRecipe: (_, DeleteRecipeInput) => {
+      return RecipeModel.findByIdAndRemove(DeleteRecipeInput.input.id)
+        .exec()
+        .then((recipe) => {
+          const RecipeStatus = { message: null };
+
+          if (recipe) {
+            RecipeStatus.message = `Recipe deleted (${DeleteRecipeInput.input.id})`;
+          } else {
+            RecipeStatus.message = `Recipe not found (${DeleteRecipeInput.input.id})`;
+          }
+
+          return RecipeStatus;
+        })
+        .then(null, err => err);
+    },
   },
 };
 
