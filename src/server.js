4800e4ab5f419e7c6b3d1e0228363dfb7c7d1e33
Alberto Bravi <albertobravi@users.noreply.github.com>
2018-10-31 17:50:22 +0100

    Revert "chore: update template engine and javascript"
    This reverts commit 943f00ed85fb3fd92a292493d666d9387ac74676.

--- src/server.js
+++ src/server.js
@@ -1,28 +1,22 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
-import RecipeSchema from "./schemas/Recipe";
-import exphbs from "express-handlebars";
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import RecipeSchema from './schemas/Recipe';
 
 const app = express();
 
-app.set("port", 8888);
+app.set('port', 8888);
 
-app.engine("handlebars", exphbs({ defaultLayout: "index" }));
-app.set("view engine", "handlebars");
+app.set('view engine', 'pug');
 
-app.use(
-  "/graphql",
-  bodyParser.json(),
-  graphqlExpress({ schema: RecipeSchema })
-);
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: RecipeSchema }));
 
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-app.use("/", (req, res) => {
-  res.render("index");
+app.use('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(app.get("port"), () => {
-  console.log(`Now browse to http://localhost:${app.get("port")}/graphiql`);
+app.listen(app.get('port'), () => {
+  console.log(`Now browse to http://localhost:${app.get('port')}/graphiql`);
 });
